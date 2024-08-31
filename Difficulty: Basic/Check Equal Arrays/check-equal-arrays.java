7fce17caf12da5adf67a9a7025bb96d54f557ae4
Harsh Aditya <153895524+creativeadi@users.noreply.github.com>
2024-08-31 13:05:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Check Equal Arrays/check-equal-arrays.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine().trim());
+
+        while (t-- > 0) {
+            String line1 = read.readLine().trim();
+            String[] numsStr1 = line1.split(" ");
+            int[] arr1 = new int[numsStr1.length];
+            for (int i = 0; i < numsStr1.length; i++) {
+                arr1[i] = Integer.parseInt(numsStr1[i]);
+            }
+
+            String line2 = read.readLine().trim();
+            String[] numsStr2 = line2.split(" ");
+            int[] arr2 = new int[numsStr2.length];
+            for (int i = 0; i < numsStr2.length; i++) {
+                arr2[i] = Integer.parseInt(numsStr2[i]);
+            }
+
+            Solution ob = new Solution();
+            System.out.println(ob.check(arr1, arr2) ? "true" : "false");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Function to check if two arrays are equal or not.
+    public static boolean check(int[] arr1, int[] arr2) {
+        // Your code here
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        return Arrays.equals(arr1, arr2);
+    }
+}
