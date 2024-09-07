cb41001fccf8bb7e1daedbf7f0fb573744ece320
Harsh Aditya <153895524+creativeadi@users.noreply.github.com>
2024-09-07 18:53:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Rotate Array by One/rotate-array-by-one.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+// // Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+            Solution ob = new Solution();
+            ob.rotate(arr);
+            for (int num : arr) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// // User function Template for Java
+
+class Solution {
+    public void rotate(int[] arr) {
+        // code here
+        int l = arr.length;
+        int last = arr[l-1];
+        for (int i=l-2; i>=0; i--){
+            arr[i+1]=arr[i];
+        }
+        arr[0] = last;
+    }
+}
