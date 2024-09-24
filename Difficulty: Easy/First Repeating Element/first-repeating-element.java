5d82d55403fe4e7e94c98e440e861a17fe8ecf71
Harsh Aditya <153895524+creativeadi@users.noreply.github.com>
2024-09-24 23:41:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/First Repeating Element/first-repeating-element.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Long> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Long.parseLong(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (long i : array) arr[idx++] = (int)i;
+
+            Solution obj = new Solution();
+
+            // calling maxSubarraySum() function
+            System.out.println(obj.firstRepeated(arr));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Function to return the position of the first repeating element.
+    public static int firstRepeated(int[] arr) {
+        // Your code here
+        HashSet<Integer> h=new HashSet<>();
+        int index=-1;
+        for(int i=arr.length-1;i>=0;i--){
+            if(h.contains(arr[i])){
+                index=i;
+            }
+            else{
+                h.add(arr[i]);
+            }
+        }
+        return index==-1?-1:index+1;
+    }
+}
